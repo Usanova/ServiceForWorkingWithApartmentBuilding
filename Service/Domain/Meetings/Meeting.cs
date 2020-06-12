daa640b8b089a82b13dddc801a328dae9ddbf8dc
atiss16 <el_eve_n@mail.ru>
2020-06-12 16:07:00 +0500

    Add meeting

---
+++ Service/Domain/Meetings/Meeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Meetings
+{
+    public sealed class Meeting
+    {
+        public Guid MeetingId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Guid OwnerId { get; private set; }
+    }
+}
---
+++ Service/Infrastructure/Meetings/MeetingDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Domain.Meetings;
+
+namespace Infrastructure.Meetings
+{
+    public sealed class MeetingDbContext : DbContext
+    {
+        public MeetingDbContext(DbContextOptions<MeetingDbContext> options) : base(options)
+        { }
+
+        public DbSet<Meeting> Meetings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Meeting>(m =>
+            {
+                m.ToTable("Meeting");
+                m.HasKey(m => m.MeetingId);
+            });
+        }
+    }
+}
